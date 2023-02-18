c27f4d0d4c3a178d008e1d832c6628744c604a3b
GabrielRQueiroz <gabrielr.queiroz@gmail.com>
2023-02-17 21:56:37 -0300

    🐞 fix(aws):
    Não acredito que errei o comando!!!!!!!!

--- server/.platform/hooks/postdeploy/00_get_certificate.sh
+++ server/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d YOUR_ELASTICBEANSTALK_URL_HERE --nginx --agree-tos --email YOUR_EMAIL
+sudo certbot -n -d cjrwallserver.eba-tuwdvavz.sa-east-1.elasticbeanstalk.com --nginx --agree-tos --email gabrielr.queiroz@gmail.com
